3b5536355efda4667652c5cb2ad325af122b011e
Hayrullah Cansu <hayrullah.cansu@cdev.global>
2019-11-01 14:47:57 +0300

    connection keep alive

--- core/comm/netw/base_player_client.go
+++ core/comm/netw/base_player_client.go
@@ -15,7 +15,7 @@ const (
 	writeWait = 10 * time.Second
 
 	// Time allowed to read the next pong message from the peer.
-	pongWait = 60 * time.Second
+	pongWait = 30 * time.Second
 
 	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
@@ -169,6 +169,11 @@ func (c *BaseClient) ReadPump() {
 // // application ensures that there is at most one writer to a Connection by
 // // executing all writes from this goroutine.
 func (c *BaseClient) WritePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.Conn.Close()
+	}()
 	for {
 		select {
 		case message, ok := <-c.Send:
@@ -191,7 +196,11 @@ func (c *BaseClient) WritePump() {
 				log.Println(err)
 				return
 			}
-			// default:
+		case <-ticker.C:
+			c.Conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.Conn.WriteMessage(websocket.PingMessage, []byte{}); err != nil {
+				return
+			}
 		}
 	}
 }
